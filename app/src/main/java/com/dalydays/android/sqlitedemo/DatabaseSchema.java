103d349578e9088b88bb6bfccded4bcde6a1daeb
linucksrox <ericdaly.aplus@gmail.com>
2016-08-16 06:23:23 -0400

    Add DatabaseShema.java

---
+++ app/src/main/java/com/dalydays/android/sqlitedemo/DatabaseSchema.java
@@ -0,0 +1,17 @@
+package com.dalydays.android.sqlitedemo;
+
+/**
+ * Created by eric on 8/15/16.
+ */
+public class DatabaseSchema {
+    public final class DatabaseTable {
+        public static final String NAME = "tableName";
+
+        public final class Cols {
+            public static final String _ID = "_id";
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+        }
+    }
+}
