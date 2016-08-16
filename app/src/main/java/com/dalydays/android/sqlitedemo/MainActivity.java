c71c27388ef4b42d6edd33c82a7bc66aafc73904
linucksrox <ericdaly.aplus@gmail.com>
2016-08-16 07:00:34 -0400

    Add more to MainActivity

--- app/src/main/java/com/dalydays/android/sqlitedemo/MainActivity.java
+++ app/src/main/java/com/dalydays/android/sqlitedemo/MainActivity.java
@@ -1,10 +1,12 @@
 package com.dalydays.android.sqlitedemo;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+import com.dalydays.android.sqlitedemo.CommentDatabaseSchema.CommentTable;
 
 public class MainActivity extends AppCompatActivity {
     private EditText titleEditText;
@@ -27,5 +29,19 @@ public class MainActivity extends AppCompatActivity {
     public static void addCommentButtonClick(Context context) {
         // Add the comment to the database, update the list of comments,
         // and clear the existing EditTexts
+    }
+
+    private ContentValues getContentValues() {
+        ContentValues values = new ContentValues();
+        values.put(CommentTable.Cols.UUID, )
+        values.put(CommentTable.Cols.TITLE, titleEditText.getText().toString());
+        values.put(CommentTable.Cols.COMMENT, commentEditText.getText().toString());
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        db.close();
     }
 }
