bf92285dfe785306c2010712db0f0f7732fe99a9
linucksrox <ericdaly.aplus@gmail.com>
2016-08-16 06:31:27 -0400

    Update/rename schema

--- app/src/main/java/com/dalydays/android/sqlitedemo/CommentDatabaseSchema.java
+++ app/src/main/java/com/dalydays/android/sqlitedemo/CommentDatabaseSchema.java
@@ -3,14 +3,14 @@ package com.dalydays.android.sqlitedemo;
 /**
  * Created by eric on 8/15/16.
  */
-public class DatabaseSchema {
-    public final class DatabaseTable {
+public class CommentDatabaseSchema {
+    public final class CommentTable {
         public static final String NAME = "tableName";
 
         public final class Cols {
-            public static final String _ID = "_id";
             public static final String UUID = "uuid";
             public static final String TITLE = "title";
+            public static final String COMMENT = "comment";
             public static final String DATE = "date";
         }
     }
